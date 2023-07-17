a633901132d88c1a747c4160b80d39aaff4986d8
Oabavo <onotuabavo@yahoo.com>
2023-07-16 17:35:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - This is a building a multilingual puzzle using pits function
- * Return: 0
+ * Return: o
  */
 int main(void)
 {
 	puts(" \"Programming is like building a multilingual puzzle");
-	return (0);
+	return (o);
 }
